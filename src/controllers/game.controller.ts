0e2d3afacedff5c6814a0526f4564137eeafee7a
DanielCs88 <daniel.cs20@gmail.com>
2018-09-10 13:42:17 +0200

    Game Controller up to YAML specs.

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,5 +1,5 @@
-import {Request, Response } from 'express';
-import {controller, httpGet, interfaces} from "inversify-express-utils";
+import { Response } from 'express';
+import {controller, httpGet, interfaces, requestParam, response} from "inversify-express-utils";
 import {inject} from "inversify";
 import {GameService} from "../services/game.service";
 
@@ -9,7 +9,12 @@ export class GameController implements interfaces.Controller {
     constructor(@inject('GameService') private gameService: GameService) { }
 
     @httpGet('/:size')
-    private get(req: Request, res: Response) {
-        
+    private startNewGame(@requestParam('size') size: number, @response() res: Response) {
+        try {
+            return this.gameService.getNewGame(size);
+        } catch (error) {
+            res.status(400);
+            return 'Invalid input';
+        }
     }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,6 +1,23 @@
 import { injectable } from "inversify";
+import { GameModel } from "../models/game.model";
+
+const CARD_NUMBER_LOWER_LIMIT = 2;
+const CARD_NUMBER_UPPER_LIMIT = 20;
 
 @injectable()
 export class GameService {
 
+    readonly getNewGame = (size: number): GameModel => {
+        if (!this.isNumber(size) || size < CARD_NUMBER_LOWER_LIMIT || size > CARD_NUMBER_UPPER_LIMIT) {
+            throw new Error('Invalid stack size!');
+        }
+        return {
+            token: 'asd',
+            pictures: ['card1', 'card2']
+        };
+    };
+
+    private readonly isNumber = (num: any) => {
+        return !isNaN(+num) && isFinite(num);
+    };
 }

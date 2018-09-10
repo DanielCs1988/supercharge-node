ca143d6202cefb216583793c33be1b98790ed527
DanielCs88 <daniel.cs20@gmail.com>
2018-09-10 13:51:32 +0200

    Score Controller up to YAML specs.

--- src/controllers/score.controller.ts
+++ src/controllers/score.controller.ts
@@ -1,7 +1,8 @@
-import {Request, Response } from 'express';
-import {controller, httpGet, interfaces} from "inversify-express-utils";
+import {controller, httpGet, httpPost, interfaces, requestBody} from "inversify-express-utils";
 import {ScoreService} from "../services/score.service";
 import {inject} from "inversify";
+import {ScoreRequestModel} from "../models/score-request.model";
+import {ScoreResponseModel} from "../models/score-response.model";
 
 @controller('/score')
 export class ScoreController implements interfaces.Controller {
@@ -9,7 +10,12 @@ export class ScoreController implements interfaces.Controller {
     constructor(@inject('ScoreService') private scoreService: ScoreService) { }
 
     @httpGet('/')
-    private get(req: Request, res: Response) {
+    private getHighScores() {
+        return this.scoreService.getHighScores();
+    }
 
+    @httpPost('/')
+    private submitScore(@requestBody() score: ScoreRequestModel): ScoreResponseModel {
+        return this.scoreService.saveScore(score);
     }
 }
--- src/services/score.service.ts
+++ src/services/score.service.ts
@@ -1,6 +1,24 @@
 import { injectable } from "inversify";
+import {ScoreModel} from "../models/score.model";
+import {ScoreRequestModel} from "../models/score-request.model";
+import {ScoreResponseModel} from "../models/score-response.model";
 
 @injectable()
 export class ScoreService {
 
+    getHighScores = (): ScoreModel[] => {
+        return [
+            {
+                "steps": 0,
+                "seconds": 0,
+                "name": "string"
+            }
+        ];
+    };
+
+    saveScore = (score: ScoreRequestModel): ScoreResponseModel => {
+        return {
+            "position": 0
+        };
+    };
 }
